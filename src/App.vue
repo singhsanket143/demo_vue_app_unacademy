4d23162e369af7861563d891b80586abc596d45f
Sanket Singh <singhsanket143@gmail.com>
2021-08-12 12:36:45 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>My Todos APP</h1>
+    <ul>
+      <li v-for="post of posts" :key="post.id"> {{post.title}} </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from "axios";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async created() {
+    try {
+      const res = await axios.get("http://localhost:3000/api/post");
+      console.log(res);
+      this.posts = res.data.data;
+    } catch(e) {
+      console.error(e);
+    }
   }
 }
 </script>
